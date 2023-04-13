2116f521c093ac2c692491b138359eb6ef05b232
divagarsethuraman <130477536+divagarsethuraman@users.noreply.github.com>
2023-04-13 15:42:25 +0530

    Update Graph of a Function.py

--- Graph of a Function.py
+++ Graph of a Function.py
@@ -1,3 +1,7 @@
+####################################
+## Graph ploting of Real function ##
+####################################
+
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
